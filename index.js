3e30c4484d9da82bde0b94b57ae0bc33b0c33b03
Lavan Premkumar <lpremkumar@umass.edu>
2024-07-29 21:37:43 -0400

    !book function is all done

--- index.js
+++ index.js
@@ -3,22 +3,36 @@ const { token } = require('./config.json')
 const cheerio = require('cheerio')
 
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]})
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log in to Discord with your client's token
 client.login(token);
 
 client.on('error', error => {
     console.error('Discord client encountered an error:', error);
 });
+
+client.on("messageCreate", (message) => {
+try {
+    if (message.content.startsWith('!book')) {
+        bookSummary(message)
+    }
+    if (message.content.startsWith('!rate')) {
+        rate(message)
+    }
+} catch (error) {
+    console.error('Error handling message: ', error);
+    message.reply("Sorry, an error occurred while searching for the book.");
+}
+});
+
+
+const rate = async (message) => {
+    
+}
 
 const bookSummary = async (message) => {
     const query = message.content.slice(6);
@@ -37,11 +51,20 @@ const bookSummary = async (message) => {
         const bookTitle = $$('h1[data-testid="bookTitle"]').text().trim();
         const author = $$('span[data-testid="name"]').first().text().trim();
         const publishDate = $$('[data-testid="publicationInfo"]').text().trim();
+        const firstGenre = $$('.BookPageMetadataSection__genreButton').first().text().trim();
+        const secondGenre = $$('.BookPageMetadataSection__genreButton').eq(1).text().trim();
+        const thirdGenre = $$('.BookPageMetadataSection__genreButton').eq(2).text().trim();
         const synopsis = $$('[data-testid="description"]').text().trim();
         
-        console.log("Extracted data:", { bookTitle, author, publishDate, synopsis });
+        console.log("Extracted data:", { bookTitle, author, publishDate, firstGenre, secondGenre, thirdGenre, synopsis });
 
-        message.reply(`${bookTitle} by ${author} was ${publishDate}.`);
+        const messageReply = `${bookTitle} by ${author} was ${publishDate}.\nGenres: ${firstGenre}, ${secondGenre}, ${thirdGenre}\nSynopsis: ${synopsis}`
+        
+        if (messageReply.length <= 2000) {
+            message.reply(`${bookTitle} by ${author} was ${publishDate}.\nGenres: ${firstGenre}, ${secondGenre}, ${thirdGenre}\nSynopsis: ${synopsis}`);
+        } else {
+            message.reply(`${messageReply.slice(0,1997)}...`)
+        }
     } else {
         message.reply("Sorry, I couldn't find that book.");
     }
@@ -49,16 +72,6 @@ const bookSummary = async (message) => {
 
 }
 
-client.on("messageCreate", (message) => {
-    try {
-        if (message.content.startsWith('!book')) {
-            bookSummary(message)
-        }
-    } catch (error) {
-        console.error('Error handling message: ', error);
-        message.reply("Sorry, an error occurred while searching for the book.");
-    }
-});
 
 
 
